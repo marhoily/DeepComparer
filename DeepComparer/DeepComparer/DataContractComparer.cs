49f654dc9dc87af5df2301072b868ac2b0a5b0c3
Ilya <marhoily@hotmail.com>
2016-07-20 13:48:15 +0300

    initial

---
+++ DeepComparer/DeepComparer/DataContractComparer.cs
@@ -0,0 +1,16 @@
+﻿namespace DeepComparer
+{
+    public sealed class DataContractComparer
+    {
+        public bool Compare(object x, object y)
+        {
+            foreach (var p in x.GetType().GetProperties())
+            {
+                var xV = p.GetValue(x, null);
+                var yV = p.GetValue(y, null);
+                if (!Equals(xV, yV)) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ DeepComparer/Tests/DataContractComparerFacts.cs
@@ -0,0 +1,32 @@
+﻿using DeepComparer;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests
+{
+    public class DataContractComparerFacts
+    {
+        private DataContractComparer _comparer;
+
+        [Fact]
+        public void When_Different_Prop_Should_False()
+        {
+            _comparer = new DataContractComparer();
+            _comparer.Compare(new X { I = 3 }, new X { I = 4 })
+                .Should().BeFalse();
+        }
+        [Fact]
+        public void When_Same_Prop_Should_True()
+        {
+            _comparer = new DataContractComparer();
+            _comparer.Compare(new X { I = 3 }, new X { I = 3 })
+                .Should().BeTrue();
+        }
+        [DataContract]
+        public class X
+        {
+            [DataMember]
+            public int I { get; set; }
+        }
+    }
+}
