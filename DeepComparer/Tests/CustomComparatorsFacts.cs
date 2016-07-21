24cfe1a863255216e38c105dcae9ddca8ef66d30
Ilya <marhoily@hotmail.com>
2016-07-21 12:40:22 +0300

    refactor

--- DeepComparer/Tests/CustomComparatorsFacts.cs
+++ DeepComparer/Tests/CustomComparatorsFacts.cs
@@ -17,14 +17,16 @@ namespace Tests
         public void Custom_False()
         {
             _comparer
+                .DelveInto(t => t == typeof(X))
                 .RuleFor<int>((x, y) => Math.Abs(x - y) < 2)
-                .Compare(new X { I = 3 }, new X { I = 40 }, typeof(X))
-                .Should().BeFalse();
+                .Compare(new X { I = 3 }, new X { I = 4 }, typeof(X))
+                .Should().BeTrue();
         }
         [Fact]
         public void Custom_True()
         {
             _comparer
+                .DelveInto(t => t == typeof(X))
                 .RuleFor<int>((x, y) => Math.Abs(x - y) < 2)
                 .Compare(new X { I = 3 }, new X { I = 40 }, typeof(X))
                 .Should().BeFalse();
