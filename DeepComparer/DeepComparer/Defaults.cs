1a27da71d8557500dd22447d2a8ea2097c8c20b0
Ilya <marhoily@hotmail.com>
2016-07-21 15:45:48 +0300

    refactor

---
+++ DeepComparer/DeepComparer/Defaults.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeepComparison
+{
+    public static class Defaults
+    {
+        public static TreatObjectAs.Collection Array(Type t)
+        {
+            return !t.IsArray ? null : new TreatObjectAs.Collection(
+                CollectionComparisonKind.Equal, 
+                t.GetElementType(), x => (IEnumerable)x);
+        }
+        public static TreatObjectAs.Collection List(Type t)
+        {
+            if (!t.IsGenericType) return null;
+            if (t.GetGenericTypeDefinition() != typeof(List<>)) return null;
+            return new TreatObjectAs.Collection(
+                CollectionComparisonKind.Equal, 
+                t.GetGenericArguments()[0], 
+                x => (IEnumerable)x);
+        }
+        public static TreatObjectAs.Collection HashSet(Type t)
+        {
+            if (!t.IsGenericType) return null;
+            if (t.GetGenericTypeDefinition() != typeof(HashSet<>)) return null;
+            return new TreatObjectAs.Collection(
+                CollectionComparisonKind.Equivalent, 
+                t.GetGenericArguments()[0], 
+                x => (IEnumerable)x);
+        }
+        public static TreatObjectAs.Collection Enumerable(Type t)
+        {
+            var ifc = t.GetInterfaces().SingleOrDefault(x =>
+                x.IsGenericType &&
+                x.GetGenericTypeDefinition() == typeof(IEnumerable<>));
+            if (ifc == null) return null;
+            return new TreatObjectAs.Collection(
+                CollectionComparisonKind.Equal,
+                ifc.GetGenericArguments()[0],
+                x => (IEnumerable)x);
+        }
+    }
+}
--- DeepComparer/Tests/ExpandObjectFacts.cs
+++ DeepComparer/Tests/ExpandObjectFacts.cs
@@ -10,7 +10,7 @@ namespace Tests
     {
         private readonly DeepComparer _comparer =
             new DeepComparerBuilder()
-                .ExpandObjects(t => t == typeof(X))
+                .ExpandObjects(t => t.HasAttribute<DataContractAttribute>())
                 .SelectProperties(p => p.HasAttribute<DataMemberAttribute>())
                 .Build();
 
@@ -28,8 +28,26 @@ namespace Tests
             var b = new X { J = 2 };
             _comparer.Compare(a, b).Should().BeTrue();
         }
+        [Fact]
+        public void Deep_Equal()
+        {
+            var a = new X { Px = new X { I = 3 } };
+            var b = new X { Px = new X { I = 3 } };
+            _comparer.Compare(a, b).Should().BeTrue();
+        }
+        [Fact]
+        public void Deep_Different()
+        {
+            var a = new X { Px = new X { I = 3 } };
+            var b = new X { Px = new X { I = 4 } };
+            _comparer.Compare(a, b).Should().BeFalse();
+        }
+
+        [DataContract]
         public class X
         {
+            [DataMember]
+            public X Px { get; set; }
             [DataMember]
             public int I { get; set; }
             public int J { get; set; }
